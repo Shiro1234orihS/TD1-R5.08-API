dfdb41a1caf6f601b34e2559e3018ae15a76035d
Shiro1234orihS <96548862+Shiro1234orihS@users.noreply.github.com>
2024-09-28 13:41:28 +0200

    j'ai fais le visuel du la page pour crée un produit

---
+++ Client/Services/WSMarque.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using Client.Models;
+
+namespace Client.Services
+{
+    public class WSMarque
+    {
+        private static readonly HttpClient Client = new HttpClient { BaseAddress = new Uri("https://localhost:7132/api/") };
+
+        public WSMarque()
+        {
+            Client.DefaultRequestHeaders.Accept.Clear();
+            Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public async Task<List<Marque>> GetMarqueAsync(string nomControleur)
+        {
+            try
+            {
+                return await Client.GetFromJsonAsync<List<Marque>>(nomControleur);
+            }
+            catch (Exception ex)
+            {
+                // Log the exception or throw
+                return null;
+            }
+        }
+    }
+}
---
+++ Client/Services/WSTypeProduit.cs
@@ -0,0 +1,30 @@
+﻿using Client.Models;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+
+namespace Client.Services
+{
+    public class WSTypeProduit
+    {
+        private static readonly HttpClient Client = new HttpClient { BaseAddress = new Uri("https://localhost:7132/api/") };
+
+        public WSTypeProduit()
+        {
+            Client.DefaultRequestHeaders.Accept.Clear();
+            Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public async Task<List<TypeProduit>> GetMarqueAsync(string nomControleur)
+        {
+            try
+            {
+                return await Client.GetFromJsonAsync<List<TypeProduit>>(nomControleur);
+            }
+            catch (Exception ex)
+            {
+                // Log the exception or throw
+                return null;
+            }
+        }
+    }
+}
