ee05e39c078826cbd1706bafa9ff7ae0b1eabfe5
Shiro1234orihS <ricardonunesemilio@gmail.com>
2024-10-16 10:45:20 +0200

    j'ai ajouter le mapper de typeproduit

---
+++ TD1_code/Models/AutoMapper/MapperTypeProduit.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using TD1_code.Models.EntityFramework;
+using TD1_code.Models.DTO;
+
+namespace TD1_code.Models.AutoMapper
+{
+    public class MapperTypeProduit : Profile
+    {
+        public MapperTypeProduit()
+        {
+            CreateMap<TypeProduit, TypeProduitDto>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.IdTypeProduit))
+                .ForMember(dest => dest.Nom, opt => opt.MapFrom(src => src.NomTypeProduit))
+        }
+    }
+}
